e0d993d690a5cf0b1297d5e20e2f210a8db08208
vandanasuryavanshi <shivayvandana@gmail.com>
2026-01-21 19:43:07 +0530

    uploading first file

---
+++ python_basic.py
@@ -0,0 +1,13 @@
+##print basic hello world program take input and print output
+name=input("enter your name >> ")
+
+def greet(name):
+    print(f"hello {name} \n how are you ? \n good day ahead thanks")
+    return "Thank you :)"
+
+
+res=greet(name)
+print(res)
+
+
+### here we are going to do  create 
